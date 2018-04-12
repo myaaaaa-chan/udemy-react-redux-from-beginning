90f8371aa41187c17e274b9af93ad99dadfd75b4
myaaaaa.chan <myaaaaa.chan@gmail.com>
2018-04-12 10:51:16 +0900

    セクション3-チャプター35までのコード

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,88 +1,27 @@
-import React, {Component} from 'react';
-import _ from 'lodash';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
 
-import SearchFrom from './SearchFrom';
-import GeocodeResult from './GeocodeResult';
-import Map from './Map';
-import {geocode} from "../domain/GeoCorder";
-import {hotelSearch} from "../domain/HotelSearch";
-import HotelsTable from "./HotelesTable";
+import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 
-const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: {lat: 35.685462, lng: 139.752904},
-      hotels: [],
-      sortKey: 'minCharge',
-    };
-  }
-
-  handlePlaceSubmit(place) {
-    console.log(place);
-    geocode(place)
-      .then(({status, address, location}) => {
-        switch (status) {
-          case 'OK':
-            this.setState({
-              address: address,
-              location: location,
-            });
-            return hotelSearch(location);
-          case 'ZERO_RESULTS':
-            this.setErrorMessage('結果が見つかりませんでした');
-            break;
-          default:
-            break;
-        }
-
-        return [];
-      })
-      .then((hotels) => {
-        this.setState({hotels: sortedHotels(hotels, this.state.sortKey)});
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setErrorMessage('通信に失敗しました');
-      });
-  }
-
-  setErrorMessage(message) {
-    this.setState({
-      address: message,
-      location: {lat: 0, lng: 0}
-    });
-  }
-
-  handleSortKeyChange(sortKey) {
-    this.setState({sortKey, hotels: sortedHotels(this.state.hotels, sortKey)});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1 className="app-title">ホテル経度検索</h1>
-        <SearchFrom onSubmit={(place) => this.handlePlaceSubmit(place)}/>
-        <div className="result-area">
-          <Map location={this.state.location}/>
-          <div className="result-right">
-            <GeocodeResult
-              address={this.state.address}
-              location={this.state.location}
-            />
-            <h2>ホテル検索結果</h2>
-            <HotelsTable
-              hotels={this.state.hotels}
-              sortKey={this.state.sortKey}
-              onSort={(sortKey) => this.handleSortKeyChange(sortKey)}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="app">
+      <ul className="left-nav">
+        <li><Link to="/">ホテル検索</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={SearchPage} />
+        <Route path="/about" component={AboutPage} />
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App;
---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+
+import SearchFrom from './SearchFrom';
+import GeocodeResult from './GeocodeResult';
+import Map from './Map';
+import {geocode} from "../domain/GeoCorder";
+import {hotelSearch} from "../domain/HotelSearch";
+import HotelsTable from "./HotelesTable";
+
+const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
+
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {lat: 35.685462, lng: 139.752904},
+      hotels: [],
+      sortKey: 'minCharge',
+    };
+  }
+
+  handlePlaceSubmit(place) {
+    console.log(place);
+    geocode(place)
+      .then(({status, address, location}) => {
+        switch (status) {
+          case 'OK':
+            this.setState({
+              address: address,
+              location: location,
+            });
+            return hotelSearch(location);
+          case 'ZERO_RESULTS':
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          default:
+            break;
+        }
+
+        return [];
+      })
+      .then((hotels) => {
+        this.setState({hotels: sortedHotels(hotels, this.state.sortKey)});
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setErrorMessage('通信に失敗しました');
+      });
+  }
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      location: {lat: 0, lng: 0}
+    });
+  }
+
+  handleSortKeyChange(sortKey) {
+    this.setState({sortKey, hotels: sortedHotels(this.state.hotels, sortKey)});
+  }
+
+  render() {
+    return (
+      <div className="search-page">
+        <h1 className="app-title">ホテル経度検索</h1>
+        <SearchFrom onSubmit={(place) => this.handlePlaceSubmit(place)}/>
+        <div className="result-area">
+          <Map location={this.state.location}/>
+          <div className="result-right">
+            <GeocodeResult
+              address={this.state.address}
+              location={this.state.location}
+            />
+            <h2>ホテル検索結果</h2>
+            <HotelsTable
+              hotels={this.state.hotels}
+              sortKey={this.state.sortKey}
+              onSort={(sortKey) => this.handleSortKeyChange(sortKey)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
